2fb90949e184f590dcba4ef674fe8a52fbf6a802
Armanni A-C <Armanni@Armannis-MacBook-Pro.local>
2018-03-02 15:31:17 +0800

    reviews scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301035349) do
+ActiveRecord::Schema.define(version: 20180302060614) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
